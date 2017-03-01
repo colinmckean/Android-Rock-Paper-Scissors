c6f133b894c93766f59b8d1b5db8042972dfd874
Colin McKean <colinmckeandigital@gmail.com>
2017-03-01 18:06:15 +0000

    add alot of basic styling to welcome activity.

--- app/src/main/java/com/codeclan/example/rockpaperscissors/ResultActivity.java
+++ app/src/main/java/com/codeclan/example/rockpaperscissors/ResultActivity.java
@@ -7,18 +7,22 @@ import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
     TextView results;
+    TextView playerChoiceTextView;
+    TextView computerChoiceTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         results = (TextView) findViewById(R.id.game_Rslt_TextView);
+        playerChoiceTextView = (TextView) findViewById(R.id.player_choice_TextView);
+        computerChoiceTextView = (TextView) findViewById(R.id.computer_choice_TextView);
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         String result = extras.getString("result");
-      String playerChoice = extras.getString("playerChoice");
+        String playerChoice = extras.getString("playerChoice");
         String computerChoice = extras.getString("computerChoice");
         results.append(result);
-        results.append(playerChoice);
-        results.append(computerChoice);
+        playerChoiceTextView.append(playerChoice);
+        computerChoiceTextView.append(computerChoice);
     }
 }
